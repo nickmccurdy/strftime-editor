a04068e7f21dc5a752c63e42f32c8ef4bb4f076c
Nicolas McCurdy <thenickperson@gmail.com>
2014-02-04 23:15:11 -0500

    Also update the output field on keypresses and any other input

--- scripts.js
+++ scripts.js
@@ -3,6 +3,16 @@
 var input_element = document.querySelector("#input");
 var output_element = document.querySelector("#output");
 
-setInterval(function () {
+// Update the output field with the current strftime results
+var update = function () {
   output_element.value = strftime(input_element.value);
-}, 1000);
+};
+
+// Update after any changes in the input (for newer browsers)
+input_element.addEventListener("input", update);
+
+// Update after keypresses in the input (for older browsers)
+input_element.addEventListener("keypress", update);
+
+// Force an update every second (to keep the output up to date as time passes)
+setInterval(update, 1000);
